19266ce1d0437b040c11c71c029be32c265fad8c
Jake Benedick <jake_benedick@comcast.net>
2019-08-27 18:41:57 -0400

    Added div to display data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class FormApp extends React.Component {
     constructor() {
         super();
+        this.state = {};
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -11,26 +12,36 @@ class FormApp extends React.Component {
         event.preventDefault();
         const data = new FormData(event.target);
 
-        fetch('/api/form-submit-url', {
-            method: 'POST',
-            body: data,
+        this.setState({
+            res: stringifyFormData(data),
         });
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="name">Enter your name</label>
-                <input id="name" name="name" type="text" />
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Enter your name</label>
+                    <input id="name" name="name" type="text" />
 
-                <label htmlFor="email">Enter your email</label>
-                <input id="email" name="email" type="email" />
+                    <label htmlFor="email">Enter your email</label>
+                    <input id="email" name="email" type="email" />
 
-                <label htmlFor="birthdate">Enter your birth date</label>
-                <input id="birthdate" name="birthdate" type="text" />
+                    <label htmlFor="birthdate">Enter your birth date</label>
+                    <input id="birthdate" name="birthdate" type="text" />
 
-                <button>Send data!</button>
-            </form>
+                    <button>Send data!</button>
+                </form>
+
+
+                {this.state.res && (
+                    <div className="res-block">
+                        <h3>Data to be sent:</h3>
+                        <pre>FormData {this.state.res}</pre>
+                    </div>
+                )}
+
+            </div>
         );
     }
 }
