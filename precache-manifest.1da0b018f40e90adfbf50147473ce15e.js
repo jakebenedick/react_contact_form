e8f4ddc39ea89ec44b3b178728d13b813e62deb7
Jake Benedick <jake_benedick@comcast.net>
2019-08-26 21:43:53 -0400

    Updates

---
+++ precache-manifest.1da0b018f40e90adfbf50147473ce15e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d9a416aac23834a9af5779fb64da3f",
+    "url": "/codedbyjake/index.html"
+  },
+  {
+    "revision": "d92a21bd1fbac4420d29",
+    "url": "/codedbyjake/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "430dbe92aabde3565f4c",
+    "url": "/codedbyjake/static/js/2.486041c3.chunk.js"
+  },
+  {
+    "revision": "d92a21bd1fbac4420d29",
+    "url": "/codedbyjake/static/js/main.5cb87021.chunk.js"
+  },
+  {
+    "revision": "fa16d95474485f5640d5",
+    "url": "/codedbyjake/static/js/runtime~main.0e528c52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codedbyjake/static/media/logo.5d5d9eef.svg"
+  }
+]);
