9d6dd1dcdb026ee644c8fb81a6ad8b109d40d897
Jake Benedick <jake_benedick@comcast.net>
2019-08-27 12:35:15 -0400

    .

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import './App.css';
 
 class App extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = { fname: '' };
+    super(props);
+    this.state = {fname: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ fname: event.target.fname });
-    }
+  handleChange(event) {
+    this.setState({value: event.target.fname});
+  }
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.fname);
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.fname);
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-              <input type="text" value={this.state.fname} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.fname} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default App;
