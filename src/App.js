0c17a04939146c82733ecc9deee111a39d75ce76
Jake Benedick <jake_benedick@comcast.net>
2019-08-27 11:54:21 -0400

    Re-added fields to form

--- src/App.js
+++ src/App.js
@@ -22,13 +22,24 @@ class App extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-            <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
+            <div className="App">
+                <p>Contact Me</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label>First Name</label>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+                    <label>Last Name</label>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+                    <label>Email</label>
+                    <input type="email" id="email" name="email" placeholder="Your email" />
+
+                    <label>Subject</label>
+                    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         );
     }
 }
